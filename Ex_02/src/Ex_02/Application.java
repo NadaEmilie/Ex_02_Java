50d0a3baff7c40137c72ee1f9fc918dcc9bb2a64
NadaEmilie <emilie.nada2306@gmail.com>
2022-10-05 23:07:02 +0200

    exemple sur casting

--- Ex_02/src/Ex_02/Application.java
+++ Ex_02/src/Ex_02/Application.java
@@ -3,7 +3,24 @@ package Ex_02;
 public class Application {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		int i=0;
+		float f=0;
+		double d=0;
+		byte b=0;
+		
+		Casting c = new Casting();
+		c.fx(i, f);
+		c.fx(b, i);
+		c.fx(b+1000000000, b);
+		//c.fx(i, d); error
+		c.fx(i, (float) d);
+		//c.fx(i+5, f+0.5); error
+		c.fx(i+5, (float) (f+0.5));
+		c.gx(b);
+		//c.gx(b+1); b+1=int error
+		c.gx(b++);
+		//c.gx(3); 3int error
 
 	}
 
--- Ex_02/src/Ex_02/Casting.java
+++ Ex_02/src/Ex_02/Casting.java
@@ -2,4 +2,8 @@ package Ex_02;
 
 public class Casting {
 
-}
+	void fx(int i, float f) {}
+	void gx(byte b) {}
+	
+	}
+
